237072f79463d940e57c21e2f6bc8c040e29a727
Attila Rajmund Nohl <attila.r.nohl@gmail.com>
2020-05-21 16:57:38 +0200

    Implement computing.

--- lib/calculator_web/controllers/compute_controller.ex
+++ lib/calculator_web/controllers/compute_controller.ex
@@ -2,6 +2,14 @@ defmodule CalculatorWeb.ComputeController do
   use CalculatorWeb, :controller
 
   def index(conn, params) do
-    render(conn, "compute.html")
+    a = String.to_integer(params["a"])
+    b = String.to_integer(params["b"])
+    result = case params["operation"] do
+      "add" -> a+b
+      "multiply" -> a*b
+    end
+    conn
+    |> assign(:result, result)
+    |> render("compute.html")
   end
 end
